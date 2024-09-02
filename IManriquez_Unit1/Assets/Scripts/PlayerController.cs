4fc2742a179c760be1eb0c212a5ff2594fb96338
Tacman123 <142944174+Tacman123@users.noreply.github.com>
2024-09-02 16:42:16 -0700

    Unit 1 Lesson D
    Hi pushing my fulling function car controlling script :)

--- IManriquez_Unit1/Assets/Scripts/PlayerController.cs
+++ IManriquez_Unit1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // rate of forward/backward movement
     public float speed = 15.0f;
+    public float turnspeed = 15.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +20,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         Debug.Log(Time.deltaTime);
-        // Vector3.forward -- > (0, 0, 1)
-        // (0, 0, 1) * Time.deltaTime + (0, 0, .016) * = (0, 0, .16)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-  
+        
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // turning vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
+
     }
 }
