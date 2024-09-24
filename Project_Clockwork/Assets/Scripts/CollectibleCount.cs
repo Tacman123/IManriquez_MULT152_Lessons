c93d1a0549440df91ec95ec9503e0822f17fdabd
Tacman123 <142944174+Tacman123@users.noreply.github.com>
2024-09-23 23:58:51 -0700

    Independent Project: Mechcanics Pt 1

---
+++ Project_Clockwork/Assets/Scripts/CollectibleCount.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class CollectibleCount : MonoBehaviour
+{
+    public TMP_Text text;
+    int count;
+
+    void Awake()
+    {
+        
+    }
+
+    void OnEnable() => Collectible.OnCollected += OnCollectibleCollected;
+    void OnDisable() => Collectible.OnCollected -= OnCollectibleCollected;
+
+    void OnCollectibleCollected()
+    {
+        text.text = (++count).ToString();
+    }
+}
