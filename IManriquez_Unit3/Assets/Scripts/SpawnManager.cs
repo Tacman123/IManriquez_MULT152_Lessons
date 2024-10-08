a766909611f8dd43b0de362d540375e932bd6fec
Tacman123 <142944174+Tacman123@users.noreply.github.com>
2024-10-07 17:39:10 -0700

    Unit 3 Lesson B
    Seriously had a lot of fun with this lesson. :100:

--- IManriquez_Unit3/Assets/Scripts/SpawnManager.cs
+++ IManriquez_Unit3/Assets/Scripts/SpawnManager.cs
@@ -6,21 +6,21 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obsPrefab;
     private Vector3 SpawnPos = new Vector3(30, 0, 0);
+    private PlayerController playerCtrl;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObs", 2, 2);
-        
-    }
+        playerCtrl = GameObject.Find("Player").GetComponent<PlayerController>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void SpawnObs()
     {
-        Instantiate(obsPrefab, SpawnPos, obsPrefab.transform.rotation);
+        if (playerCtrl.gameOver == false)
+        {
+            Instantiate(obsPrefab, SpawnPos, obsPrefab.transform.rotation);
+        }            
     }
 }
