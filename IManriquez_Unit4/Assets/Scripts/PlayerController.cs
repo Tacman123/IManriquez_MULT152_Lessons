9af6a39516c4a85c1283563b53585eb3d7cbb2fa
Tacman123 <142944174+Tacman123@users.noreply.github.com>
2024-10-30 15:14:23 -0700

    Unit 4 Lesson C
    Hi professor. This is my push for the Coroutines portion of the Unit and I was very happy with it. :)

--- IManriquez_Unit4/Assets/Scripts/PlayerController.cs
+++ IManriquez_Unit4/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rbPlayer;
     GameObject focalPoint;
-    public float speed = 10.0f;
     Renderer rendererPlayer;
+    public float speed = 10.0f;
+    public float powerUpSpeed = 10.0f;
+    public GameObject powerUpInd;
+
+    bool hasPowerUp = false;
 
     // Start is called before the first frame update
     void Start()
@@ -32,5 +36,36 @@ public class PlayerController : MonoBehaviour
         {
             rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
         }
+        powerUpInd.transform.position = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdown());
+            powerUpInd.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {        
+        if (hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with " + collision.gameObject + " with powerup set to: " + hasPowerUp);
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayDir = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(awayDir * powerUpSpeed, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpCountdown()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        powerUpInd.SetActive(false);
     }
 }
